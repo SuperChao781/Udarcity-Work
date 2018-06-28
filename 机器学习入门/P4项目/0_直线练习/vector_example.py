657fccd886b4ba3f7f63856eba9a8befab265b66
wangyuchao <scopewang@gmail.com>
2018-06-28 17:08:14 +0800

    第1次提交
    直线练习程序，建立

---
+++ 机器学习入门/P4项目/0_直线练习/vector_example.py
@@ -0,0 +1,162 @@
+from math import sqrt, acos, pi
+from decimal import Decimal, getcontext
+
+getcontext().prec = 10
+
+class Vector(object):
+    NO_UNIQUE_PARALLEL_COMPONENT_MSG = 'no unique parallel component'
+    NO_UNIQUE_ORTHOGONAL_COMPONENT_MSG = 'no unique orthogonal component'
+    CANNOT_NORMALIZE_ZERO_VECTOR_MSG = 'Cannot normalize the zero vector'
+    ONLY_DEFINED_IN_TWO_THREE_DIMS_MSG = 'only defined in to three dims'
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple([Decimal(str(x)) for x in coordinates])#坐标
+            self.dimension = len(self.coordinates)#维度
+            self.idx = 0#当前所取的元素序号（第几个坐标）
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+        
+    def __iter__(self):
+        return self
+
+    def next(self):
+        self.idx += 1
+        try:
+            return self.coordinates[self.idx - 1]
+        except IndexError:
+            self.idx = 0
+            raise StopIteration
+        # self.current += 1
+        # if self.current >= self.dimension:
+        #     raise StopIteration
+        # else:
+        #     return self.coordinates[self.current]
+
+    def __getitem__(self, item):
+        return self.coordinates[item]
+
+    
+    def plus(self, v):
+        new_coordinates = [x+y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+   
+    def minus(self, v):
+        new_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+   
+    def times_scalar(self, c):
+        new_coordinates = [Decimal(c)*x for x in self.coordinates]
+        return Vector(new_coordinates)
+
+    def magnitude(self):#向量模值计算
+        coordinates_squared = [x**2 for x in self.coordinates]
+        return Decimal(sqrt(sum(coordinates_squared)))
+
+    
+    def normalized(self):#向量标准化（获取方向向量）
+        try:
+            magnitude = self.magnitude()
+            return self.times_scalar(Decimal('1.0')/magnitude)
+        except ZeroDivisionError:
+            raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG)
+
+    
+    def dot(self, v):
+        return sum([x*y for x,y in zip(self.coordinates, v.coordinates)])
+
+    
+    def angle_with(self, v, in_degrees=False):
+        try:
+            u1 = self.normalized()
+            u2 = v.normalized()
+            angle_in_radians = acos(u1.dot(u2))
+
+            if in_degrees:
+                degrees_per_radian = 180. /pi
+                return angle_in_radians * degrees_per_radian
+            else:
+                return angle_in_radians
+
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception('Cannot compute an angle with the zero vector')
+            else:
+                raise e
+
+   
+    def is_orthogonal_to(self, v, tolerance=1e-10):
+        return abs(self.dot(v)) < tolerance
+
+   
+    def is_parallel_to(self, v):
+        return ( self.is_zero() or v.is_zero() or self.angle_with(v) == 0 or self.angle_with(v) == pi )
+
+    
+    def is_zero(self, tolerance=1e-10):
+        return self.magnitude() < tolerance
+
+    
+    def component_orthogonal_to(self, basis):
+        try:
+            projection = self.component_parallel_to(basis)
+            return self.minus(projection)
+        except Exception as e:
+            if str(e) == self.NO_UNIQUE_PARALLEL_COMPONENT_MSG:
+                raise Exception(self.NO_UNIQUE_ORTHOGONAL_COMPONENT_MSG)
+            else:
+                raise e
+
+    
+    def component_parallel_to(self, basis):
+        try:
+            u = basis.normalized()
+            weight = self.dot(u)
+            return u.times_scalar(weight)
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception(self.NO_UNIQUE_PARALLEL_COMPONENT_MSG)
+            else:
+                raise e
+
+    
+    def cross(self, v):
+        try:
+            x_1, y_1, z_1 = self.coordinates
+            x_2, y_2, z_2 = v.coordinates
+            new_coordinates = [ y_1*z_2 - y_2*z_1 ,
+                                -(x_1*z_2 - x_2*z_1),
+                                x_1*y_2 - x_2*y_1 ]
+            return Vector(new_coordinates)
+        except ValueError as e:
+            msg = str(e)
+            if msg == 'need more than 2 values to unpack':
+                self_embedded_in_r3 = Vector(self.coordinates + ('0',))
+                v_embedded_in_r3 = Vector(v.coordinates + ('0',))
+                return self_embedded_in_r3.cross(v_embedded_in_r3)
+            elif (msg == 'too many values to unpack' or msg == 'need more than 1 value to unpack'):
+                raise Exception(self.ONLY_DEFINED_IN_TWO_THREE_DIMS_MSG)
+            else:
+                raise e
+   
+    def area_of_traingle_with(self, v):
+        return self.area_of_parallelogram_with(v) / Decimal('2.0')
+
+   
+    def area_of_parallelogram_with(self, v):
+        cross_product = self.cross(v)
+        return cross_product.magnitude()
+
